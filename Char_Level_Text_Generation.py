de440d6bb24c00088cb6bc31e545e60074535d71
ShubhamPatil007 <36992441+ShubhamPatil007@users.noreply.github.com>
2020-06-09 19:20:48 +0530

    Added comments

--- Char_Level_Text_Generation.py
+++ Char_Level_Text_Generation.py
@@ -5,6 +5,8 @@ Created on Mon Jun  8 10:09:20 2020
 @author: Shubham
 """
 
+# Import libraries
+
 import numpy as np
 import re
 import pickle
@@ -16,10 +18,14 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.compat.v1.keras.layers import CuDNNLSTM
 from tensorflow.keras.models import model_from_json
 
+# Read data (Harry Potter and the Sorcerer's Stone)
+
 response = requests.get('https://www.linguistik.uzh.ch/dam/jcr:169bff5c-ac13-457b-9acb-4fe7f1ad5cb0/Harry%20Potter%20and%20the%20Sorcerer.txt')
 text = response.text
 text = re.sub('[^\x00-\x7f]', '', text)
 text = text[80:].lower()
+
+# Create required parameters
 
 chars = sorted(list(set(text.lower())))
 char_index = {char: i for i, char in enumerate(chars)}
@@ -30,7 +36,12 @@ steps = 1
 batches = 128
 epochs = 10
 
+# dump parameters in data.pkl. Will be used while generating texts
+
 pickle.dump((chars, text, char_index, index_char, seq_len), open('Saved_Files/data.pkl', 'wb'))
+
+# Create list of lists. Each list in the list is sequnce of characters stored in 'sequences' (independet variable) and 
+# respective next character stored in 'next_char' (dependent variable).
 
 def get_sequences(text, seq_len, steps):
     sequences = []
@@ -39,6 +50,11 @@ def get_sequences(text, seq_len, steps):
         sequences.append(text[i: i + seq_len])
         next_chars.append(text[i + seq_len])
     return sequences, next_chars
+
+# Encode 'sequences' and 'next_chars' into One Hot Encoding
+# layers - number of sequences, rows - number of characters in each sequence, columns - number of characters in vocabulary
+# x is a 3 dimentional matrix
+# y is a 2 dimentional list of vectors.
 
 def encode_chars(sequences, next_chars, chars):
     layers = len(sequences)
@@ -52,6 +68,9 @@ def encode_chars(sequences, next_chars, chars):
         y[i, char_index[next_chars[i]]] = 1
     return x, y
 
+# Create LSTM neural netwok with two LSTM layers and two Dense layers
+# CuDNNLSTM (Cuda enablesd LSTM, to increase computational speed) 
+
 def build_model(seq_len, vocab_size):
     model = Sequential()
     model.add(CuDNNLSTM(units = 128, return_sequences = True, input_shape = (seq_len, vocab_size)))
@@ -63,10 +82,12 @@ def build_model(seq_len, vocab_size):
     model.add(Dense(units = vocab_size, activation = 'softmax'))
     return model
 
+# Compile and Train model 
+
 def train_model(model, x, y, batches, epochs):
     model.compile(optimizer = RMSprop(lr = 0.001), loss = 'categorical_crossentropy')
     model.fit(x, y, batch_size = batches, epochs = epochs)
-    
+    # save trained model for later use
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
         json_file.write(model_json)
@@ -74,18 +95,8 @@ def train_model(model, x, y, batches, epochs):
     
     return model
 
+# Method calls
 sequences, next_chars = get_sequences(text, seq_len, steps)
 x, y = encode_chars(sequences, next_chars, chars)
-
 model = build_model(seq_len, vocab_size)
 model = train_model(model, x, y, batches, epochs)
-
-
-
-
-
-
-
-
-
-
--- Generate_Text.py
+++ Generate_Text.py
@@ -5,25 +5,37 @@ Created on Mon Jun  8 18:11:31 2020
 @author: Shubham
 """
 
+# Import libraries
+
 import numpy as np
 import random
 import pickle
 from tensorflow.keras.models import model_from_json
 
 # Deserializing model from json file
+# Load trained model
+
 with open('model.json', 'r') as json_file:
     model_json = json_file.read()
-    
 model = model_from_json(model_json)
 model.load_weights("model.h5")
 
+# Load saved parameters from data.pkl
+
 chars, text, char_index, index_char, seq_len = pickle.load(open('Saved_Files/data.pkl', 'rb'))
+
+# Encode given seed (i.e. sequence of characters) in One Hot Encoder
 
 def encode_seed(seed):
     x = np.zeros((1, seq_len, len(chars)), dtype = np.bool)
     for i, char in enumerate(seed):
         x[0, i, char_index[char]] = 1
     return x
+
+# Insert some uncetrainties to let model pick up characters with less good predictions.
+# sample() will drow random characters from our vocabulary. 
+# However, the probability for a character to be drawn will depends directly on its probability to be the next character.
+# 'temperature' is used to tune the parameters.
 
 def sample(preds, temperature = .5):
     preds = np.asarray(preds).astype('float64')
@@ -33,6 +45,8 @@ def sample(preds, temperature = .5):
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
+
+# Generate text based on random seed 
 
 def generate_text():
     start = random.randint(0, len(text) - seq_len)
@@ -47,13 +61,3 @@ def generate_text():
     return generated
 
 generate = generate_text()
-
-
-
-
-
-
-
-
-
-
